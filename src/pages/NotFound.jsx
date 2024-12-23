2573a51bd2e3d42e2dd87e51a37e215da1b52c01
Mehroz farooq <85057779+Mehroz101@users.noreply.github.com>
2024-12-23 16:29:06 +0500

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/NotFound.css"; // Optional CSS file for styling
-
 const NotFound = () => {
   return (
     <div className="not-found">
