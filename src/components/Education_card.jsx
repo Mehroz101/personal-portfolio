328c44e55c994323a7287d15f6c46097da05ce7b
Mehroz101 <mehrozfarooq127@gmail.com>
2024-12-26 18:02:18 +0500

    adjust code

--- src/components/Education_card.jsx
+++ src/components/Education_card.jsx
@@ -5,20 +5,27 @@ import { useInView } from "react-intersection-observer";
 const Education_card = ({ years, degree, university, description }) => {
   const [ref, inView] = useInView({
     triggerOnce: true, // Animation triggers only once when it enters the viewport
-    threshold: 0.2,    // Start the animation when 20% of the card is in view
+    threshold: 0.2, // Start the animation when 20% of the card is in view
   });
 
   const animationVariant = {
-    hidden: { scale: 0 , opacity:0 },
-    visible: { scale:1, opacity:1, transition: { duration: 0.1, ease: "easeIn" }  },
+    hidden: { scale: 0, opacity: 0, y: 200 },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.1, ease: "easeIn" },
+    },
   };
   return (
     <>
       <motion.div
-      ref={ref}
-      variants={animationVariant}
-      initial="hidden"
-      animate={inView ? "visible" : "hidden"} className="education_card">
+        ref={ref}
+        variants={animationVariant}
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"}
+        className="education_card"
+      >
         <span className="year">{years}</span>
         <h3 className="degree">{degree}</h3>
         <p className="university">{university}</p>
